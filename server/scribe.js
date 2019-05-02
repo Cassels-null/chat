65a0ef0ba6c7af3bd683303bfcb05570f49d83c2
Cassels-null <42cassels@gmail.com>
2019-05-02 15:32:16 -0400

    Timestamp messages

--- server/scribe.js
+++ server/scribe.js
@@ -14,16 +14,23 @@ var messageSchema = new mongoose.Schema({
     text: String
 });
 
+//define model
 var Message = mongoose.model('Message', messageSchema);
 
+//define controller
 var scribe = {
-    record : (req, res)=>{
-        var message = new Message({userName: req.body.userName, time: 1, text: req.body.text});
+    //accepts a post request with a username and text and saves it to the database
+    record : (req, res)=>{ 
+        var message = new Message({
+                userName: req.body.userName, 
+                time: Date.now(), //timestamp the message
+                text: req.body.text});
         message.save(function (err, message) {
             if (err) return console.error(err);
             res.send(201);
         });
     },
+    //responds to a request with a list off all messages in the database
     read : (req, res)=>{
         Message.find((err, messages)=>{
             if (err) return console.error(err);
