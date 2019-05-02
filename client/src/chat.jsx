ec513e36d7e5ecab96d0f8a6fb924f77f67d2aaf
Cassels-null <42cassels@gmail.com>
2019-05-02 17:19:09 -0400

    Add temporary styleing to chat and message components

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Message from './message.jsx';
 
-//react setup
+//default react setup
 class Chat extends React.Component {
     constructor(props){
         super(props);
@@ -11,9 +11,11 @@ class Chat extends React.Component {
 
     render(){
         return(<div id="chat">
+        {/* render a message component for each element in the messages prop*/}
         {this.props.messages.map((message)=>{return(
             <Message message={message}/>
         )})}
+        <div id="mark"/>{/* target for automatic scrolling. used in onComponentDidMount in messages.jsx */}
         </div>)
     }
 };
--- client/src/message.jsx
+++ client/src/message.jsx
@@ -7,8 +7,12 @@ class Message extends React.Component {
         this.state = {};
     }
 
+    componentDidMount(){//scroll down when a new message appears
+        document.getElementById("mark").scrollIntoView();
+    }
+
     render(){
-        return(<div id="message">
+        return(<div className="message">
             <div className="userName">{this.props.message.userName}</div>
             <div id="text">{this.props.message.text}</div>
         </div>)
